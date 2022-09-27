b7109fffcfe7b6725e1935bac03107f7b926e222
anaves <anaves@gmail.com>
2022-09-26 21:51:47 -0300

    Aula 26/09/2022

---
+++ projeto006/pages/mouse.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function init(){
+    // React Hooks
+    // const vetor = useState(0)
+    // let x = vetor[0]
+    // let setX = vetor[1]
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+
+    function moveMouse(evt){
+        console.log(evt.clientX, evt.clientY)
+        setX(evt.clientX)
+        setY(evt.clientY)
+    }
+
+    return (
+        <div style={{
+            height:"100vh",
+            display:"flex",
+            flexDirection:"column",
+            backgroundColor: "black",
+            color: "white",
+            alignItems: "center",
+            justifyContent: "center"
+        }} onMouseMove={moveMouse}>
+            <span>Mouse x: {x}</span>
+            <span>Mouse y: {y}</span>
+        </div>
+
+    )
+
+}
